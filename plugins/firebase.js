b8e206dad6eb7b03985e0e3fb2581e7db47915e4
yosuke.aramaki <gnx.vw903@gmail.com>
2019-10-17 17:01:50 +0900

    firebase set up

---
+++ plugins/firebase.js
@@ -0,0 +1,11 @@
+export default firebase
+
+import firebase from 'firebase'
+import 'firebase/firestore'
+const serviceAccount = require('~/../serviceAccountKey.json')
+
+firebase.initializeApp({ ...serviceAccount })
+const db = firebase.firestore()
+const settings = { timestampsInSnapshots: true }
+db.settings(settings)
+export { db }
